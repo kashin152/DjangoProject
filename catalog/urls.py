eedde7718800fab77fd4a5b3e4e26ac6f03fc65c
alexandra kashina <kashin159@gmail.com>
2024-09-27 22:07:53 +0300

    upgrade file

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,7 @@ from catalog import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("contact/", views.contact, name="contact"),
+    path(
+        "product_detail/<int:product_id>", views.product_detail, name="product_detail"
+    ),
 ]
