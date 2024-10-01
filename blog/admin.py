8ec9df7a15ef5ea2d87b4690e0ce402895d92967
alexandra kashina <kashin159@gmail.com>
2024-10-01 22:19:58 +0300

    upgrade file

---
+++ blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Blog
+
+
+@admin.register(Blog)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'publication_sign')
+    list_filter = ('title',)
+    search_fields = ('title', 'content',)
+
+
+from django.contrib import admin
+
+# Register your models here.
--- catalog/views.py
+++ catalog/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views import View
 
 from catalog.models import Product
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "base.html", context=context)
+class CatalogHomeView(ListView):
+    model = Product
+    template_name = 'catalog/base.html'
+    context_object_name = 'products'
 
 
-def contact(request):
-    if request.method == "POST":
-        # Получение данных из формы
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-        # Обработка данных
+class CatalogContactsView(View):
+    def get(self, request):
+        return render(request, 'catalog/contacts.html')
+
+    def post(self, request):
+        #Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, "catalog/contact.html")
 
 
-def product_detail(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    context = {"product": product}
-    return render(request, "product_detail.html", context=context)
+class CatalogDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
